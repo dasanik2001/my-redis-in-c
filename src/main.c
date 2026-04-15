9d8e84552442f2c823e95aaf8ce35a6b4894067b
Anik <apple@Aniks-MacBook-Air.local>
2026-04-15 22:57:18 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -69,7 +69,7 @@ char *get(struct server_data *sd, char *key)
 			time_t curr_time = time(NULL) * 1000;
 			printf("Current time: %ld\n", (long)curr_time);
 			printf("Entry TTL: %ld\n", (long)sd->entries[i].ttl);
-			if (curr_time < sd->entries[i].ttl)
+			if (curr_time > sd->entries[i].ttl)
 			{
 				// Entry has expired
 
